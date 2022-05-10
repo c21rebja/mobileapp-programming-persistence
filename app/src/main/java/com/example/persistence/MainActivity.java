475e75cdc0ab035fcb9af9429ac78ad0321f4a1c
c21rebja <c21rebja@student.his.se>
2022-05-10 16:13:32 +0200

    added all the buttons from the layout in main activity

--- app/src/main/java/com/example/persistence/MainActivity.java
+++ app/src/main/java/com/example/persistence/MainActivity.java
@@ -3,16 +3,24 @@ package com.example.persistence;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ContentValues;
+import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button readButton;
+    private Button writeButton;
+    private Button deleteButton;
 
     private SQLiteDatabase database;
     private DatabaseHelper databaseHelper;
@@ -25,9 +33,32 @@ public class MainActivity extends AppCompatActivity {
         databaseHelper = new DatabaseHelper(this);
         database = databaseHelper.getWritableDatabase();
 
+        readButton = findViewById(R.id.read_button);
+        readButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                printTrees();
+                Log.d("===", "Any trees would have been listed above.");
+            }
+        });
+
+        writeButton = findViewById(R.id.write_button);
+        writeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //add data from edit texts
+            }
+        });
+
+        deleteButton = findViewById(R.id.delete_button);
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                deleteAllTrees();
+            }
+        });
+
         //addTree("Björk", 306); //this would add something new every time we run the app
-        printTrees();
-        Log.d("===", "Any trees would have been listed above.");
     }
 
     private void deleteAllTrees () {
